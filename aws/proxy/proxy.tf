0d2682c0ce5592f7aeb44abe05b5eec5dc83d903
Riley Snyder <rileysnyder@protonmail.ch>
2023-11-16 07:13:54 -0600

    Update proxy.tf

--- aws/proxy/proxy.tf
+++ aws/proxy/proxy.tf
@@ -2,7 +2,7 @@
 resource "harness_autostopping_aws_proxy" "rdsconnectproxy" {
   name                   = substr("${local.name}-proxy", 0, 20)
   cloud_connector_id     = var.harness_cloud_connector_id
-  host_name              = var.hostedzone == "" ? "192.168.0.1.nip.op" : "${local.name}.${data.aws_route53_zone.zone[0].name}"
+  host_name              = var.hostedzone == "" ? "192.168.0.1.nip.io" : "${local.name}.${data.aws_route53_zone.zone[0].name}"
   region                 = var.region
   vpc                    = var.vpc
   security_groups        = [aws_security_group.sgforproxy.id]
